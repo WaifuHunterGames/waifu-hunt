0c918c9d8c92496699ccd55081f61fa925779c3f
Soybean <soybean3r@gmail.com>
2019-05-19 17:03:36 -0400

    Update 0.0.1
    Added Movement
    Added Scene
    Added Box Collision with rocks

---
+++ Waifu Hunt/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer sr;
+    public float moveSpeed;
+
+    void Start(){
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        moveSpeed = 3;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < 0.5f ){
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+        if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < 0.5f ){
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+        }
+    }
+}
